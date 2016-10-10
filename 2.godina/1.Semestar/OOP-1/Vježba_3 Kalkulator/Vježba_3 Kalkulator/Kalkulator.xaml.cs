d49932f190e2844c78ee5b34167f148268437b97
Roberto Kedmenec <rexerd96@gmail.com>
2016-10-10 18:43:18 +0200

    gotov projekt
    Gotovo 10102016

---
+++ 2.godina/1.Semestar/OOP-1/Vježba_3 Kalkulator/Vježba_3 Kalkulator/Kalkulator.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vježba_3_Kalkulator
+{
+    /// <summary>
+    /// Interaction logic for Kalkulator.xaml
+    /// </summary>
+    public partial class Kalkulator : Window
+    {
+        public Kalkulator()
+        {
+            InitializeComponent();
+        }
+
+        
+
+      
+        
+        private void zbrojButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            double prviBroj;
+            double drugiBroj;
+            double rezultat;
+
+            prviBroj = double.Parse(prviBrojTextBox.Text);
+            drugiBroj = double.Parse(drugiBrojTextBox.Text);
+            rezultat = prviBroj + drugiBroj;
+            rezultatBrojTextBox.Text = rezultat.ToString();
+        }
+
+        private void oduzmiButton_Click(object sender, RoutedEventArgs e)
+        {
+            double prviBroj;
+            double drugiBroj;
+            double rezultat;
+
+            prviBroj = double.Parse(prviBrojTextBox.Text);
+            drugiBroj = double.Parse(drugiBrojTextBox.Text);
+            rezultat = prviBroj - drugiBroj;
+            rezultatBrojTextBox.Text = rezultat.ToString();
+        }
+
+        private void pomnoziButton_Click(object sender, RoutedEventArgs e)
+        {
+            double prviBroj;
+            double drugiBroj;
+            double rezultat;
+
+            prviBroj = double.Parse(prviBrojTextBox.Text);
+            drugiBroj = double.Parse(drugiBrojTextBox.Text);
+            rezultat = prviBroj * drugiBroj;
+            rezultatBrojTextBox.Text = rezultat.ToString();
+        }
+
+        private void podijeliButton_Click(object sender, RoutedEventArgs e)
+        {
+            double prviBroj;
+            double drugiBroj;
+            double rezultat;
+
+            prviBroj = double.Parse(prviBrojTextBox.Text);
+            drugiBroj = double.Parse(drugiBrojTextBox.Text);
+            rezultat = prviBroj / drugiBroj;
+            rezultatBrojTextBox.Text = rezultat.ToString();
+        }
+    }
+    }
+
