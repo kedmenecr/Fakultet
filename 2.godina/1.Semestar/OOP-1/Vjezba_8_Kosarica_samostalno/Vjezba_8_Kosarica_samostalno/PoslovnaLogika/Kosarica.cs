9766a5ec12a7343d594cfffed5585169df0a6c61
Roberto Kedmenec <kedmenec.roberto@gmail.com>
2016-11-28 10:35:41 +0100

    VježbaKošarice

---
+++ 2.godina/1.Semestar/OOP-1/Vjezba_8_Kosarica_samostalno/Vjezba_8_Kosarica_samostalno/PoslovnaLogika/Kosarica.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba_8_Kosarica_samostalno.PoslovnaLogika
+{
+    class Kosarica
+    {
+    }
+}
---
+++ 2.godina/1.Semestar/OOP-1/Vjezba_8_Kosarica_samostalno/Vjezba_8_Kosarica_samostalno/PoslovnaLogika/VlasnikKosarice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba_8_Kosarica_samostalno.PoslovnaLogika
+{
+    class VlasnikKosarice
+    {
+        private string id;
+        private string naziv;
+        private string adresa;
+
+
+
+
+        public VlasnikKosarice(string id, string naziv, string adresa)
+        {
+            this.id = id;
+            this.naziv = naziv;
+            this.adresa = adresa;
+        }
+        public string vratiID(string id) {
+            return id;
+        }
+        public string vratiAdresu(string adresa)
+        {
+            return adresa;
+        }
+        public string vratiNaziv(string naziv)
+        {
+            return adresa;
+        }
+        public void promjeniNaziv(string naziv) {
+
+            this.naziv = naziv;
+        }
+        public void promjeniAdresu(string adresa)
+        {
+
+            this.adresa= adresa;
+        }
+
+    }
+}
+
+
