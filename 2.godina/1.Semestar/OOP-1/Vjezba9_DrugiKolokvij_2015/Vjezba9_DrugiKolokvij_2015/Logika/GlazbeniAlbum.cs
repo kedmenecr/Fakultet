cfe7d383e116f5238def030e66f7217e3ad5d5c3
kedmenecr <kedmenec.roberto@gmail.com>
2016-11-29 15:17:58 +0100

    Vjeza9Kolokvij_Novo

--- 2.godina/1.Semestar/OOP-1/Vjezba9_DrugiKolokvij_2015/Vjezba9_DrugiKolokvij_2015/Logika/GlazbeniAlbum.cs
+++ 2.godina/1.Semestar/OOP-1/Vjezba9_DrugiKolokvij_2015/Vjezba9_DrugiKolokvij_2015/Logika/GlazbeniAlbum.cs
@@ -8,5 +8,36 @@ namespace Vjezba9_DrugiKolokvij_2015.Logika
 {
     class GlazbeniAlbum
     {
+        private string naziv;
+        private Izvodac izvodac;
+        private Zanr zanr;
+        private List<PjesmaNaAlbumu> pjesme;
+
+        public GlazbeniAlbum(string naziv, Izvodac izvodac, Zanr zanr)
+        {
+            this.naziv = naziv;
+            this.izvodac = izvodac;
+            this.zanr = zanr;
+            pjesme = new List<PjesmaNaAlbumu>();
+        }
+        public string getNaziv() {
+            return naziv;
+        }
+        public Izvodac getIzvodac()
+        {
+            return izvodac;
+        }
+        public List<PjesmaNaAlbumu> getListaPjesma()
+        {
+            return pjesme;
+        }
+        public void dodajPjesmu(PjesmaNaAlbumu pjesme) {
+        pjesme.Add(pjesme)
+                }
+
+        ////public void DodajPjesmu(int rednibrojpjesme, string naziv, int trajanjeusekundama) {
+        ////    pjesme.Add(new PjesmaNaAlbumu(rednibrojpjesme, naziv, trajanjeusekundama));
+
+        ////}
     }
 }
