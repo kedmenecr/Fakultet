fa7107314426a69d9f7e1898ca6477b9a11fa8c0
Roberto Kedmenec <kedmenec.roberto@gmail.com>
2016-12-04 20:37:08 +0100

    Hello

---
+++ 2.godina/1.Semestar/OOP-1/2_kolovij_valjak/2_kolovij_valjak/PoslovnaLogika/Valjak.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_kolovij_valjak.PoslovnaLogika
+{
+    class Valjak
+    {
+        private float polumjer;
+        private float visina;
+
+        //konstruktor na 1
+        public Valjak() {
+            
+            polumjer = 1;
+            visina = 1;
+        }
+        //konsturktor za postavljanej
+
+        public Valjak(float polumjer , float visina) {
+            this.polumjer = polumjer;
+            this.visina = visina;
+
+        }
+        //postavi i dohvati
+        public void PostaviPolumjer(float polumjer) {        
+            this.polumjer = polumjer;
+        }
+
+        public float DohvatiPolumjer() {
+            return polumjer;
+            }
+
+        public void PostaviVisinu(float visina) {
+            this.visina = visina;
+        }
+        public float DohvatiVisinu() {
+            return visina;
+        }
+
+
+    }
+}
---
+++ 2.godina/1.Semestar/OOP-1/2_kolovij_valjak/2_kolovij_valjak/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_kolovij_valjak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
