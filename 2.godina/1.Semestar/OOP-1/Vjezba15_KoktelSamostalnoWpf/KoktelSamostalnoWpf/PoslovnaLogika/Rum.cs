6daf30502eae351ab666753c4480e1c342875670
Roberto Kedmenec <kedmenec.roberto@gmail.com>
2017-01-17 01:34:09 +0100

    ispravakOOP

---
+++ 2.godina/1.Semestar/OOP-1/Vjezba15_KoktelSamostalnoWpf/KoktelSamostalnoWpf/PoslovnaLogika/Rum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoktelSamostalnoWpf.PoslovnaLogika
+{
+    class Rum:Sastojak
+    {
+        public Rum(string naziv,double kolicina) :base(naziv,kolicina)
+        {
+            this.naziv = naziv;
+            kolicinaDeci = kolicina;
+        }
+        public override string Prikazi()
+        {
+            return kolicina + "dcl ruma Bacardi Gold";
+    }
+    }
+}
---
+++ 2.godina/1.Semestar/OOP-1/Vjezba15_KoktelSamostalnoWpf/KoktelSamostalnoWpf/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoktelSamostalnoWpf.PoslovnaLogika;
+
+namespace KoktelSamostalnoWpf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // preprema koktela
+            Koktel CubaLibre = new Koktel("Cuba Libre");
+            CubaLibre.Dodaj(new Limeta(0.5));
+            CubaLibre.Dodaj(new Rum("Baccardi gold", 0.4));
+            CubaLibre.Dodaj(new CocaCola("zero", 1));
+            CubaLibre.Dodaj(new Led("drobljeni"));
+            Console.WriteLine(CubaLibre.Posluzi());
+            Console.ReadKey();
+        }
+    }
+}
