2a319e2f7fb0b991b9140d41ec84c122cbbc8443
Roberto Kedmenec <kedmenec.roberto@gmail.com>
2017-01-17 01:21:17 +0100

    KoktelOOPSamostalno

---
+++ 2.godina/1.Semestar/OOP-1/KoktelSamostalnoWpf/KoktelSamostalnoWpf/PoslovnaLogika/Led.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoktelSamostalnoWpf.PoslovnaLogika
+{
+    class Led:Sastojak
+    {
+        public Led(string naziv) : base(naziv)
+        {
+            this.naziv = naziv;
+            kolicinaDeci = 1;
+        }
+        public override string Prikazi()
+        {
+            return "Led zdrobljeni";
+        }
+
+    }
+}
